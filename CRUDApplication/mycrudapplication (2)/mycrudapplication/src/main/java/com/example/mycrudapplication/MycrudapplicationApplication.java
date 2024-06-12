ec9be1dbbd4e17dec190dd128993b80cb77272dc
AbidaMemon <73389545+AbidaMemon@users.noreply.github.com>
2024-06-12 10:25:40 +0500

    Add files via upload

---
+++ CRUDApplication/mycrudapplication (2)/mycrudapplication/src/main/java/com/example/mycrudapplication/MycrudapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycrudapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycrudapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycrudapplicationApplication.class, args);
+	}
+
+}
---
+++ CRUDApplication/mycrudapplication (2)/mycrudapplication/src/test/java/com/example/mycrudapplication/MycrudapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycrudapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycrudapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
